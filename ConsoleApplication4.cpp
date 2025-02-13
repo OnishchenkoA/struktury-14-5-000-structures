4035b8e17217e1dce41f08af650598045573158b
Onoshchenko Alyona <eco.makarova.mgb1@mail.ru>
2025-02-14 00:42:06 +0500

    125

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,47 +1,57 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 struct address
 {
-	std::string town;
-	std::string strit;
-	int home;
-	int apartment;
-	int index;
+	std::string town[3];
+	std::string strit[3];
+	int home[3];
+	int apartment[3];
+	int index[3];
 };
+
+void print(address& add1)
+{
+	setlocale(LC_ALL, "russian");
+	address address_array;
+	address* add = &address_array;
+	for (int i = 0; i < 3; i++) {
+		std::cout << "Город: " << add[i].town << std::endl;
+		std::cout << "Улица: " << add[i].strit << std::endl;
+		std::cout << "Номер дома: " << add[i].home << std::endl;
+		std::cout << "Номер квартиры: " << add[i].apartment << std::endl;
+		std::cout << "Индекс: " << add[i].index << std::endl;
+		std::cout << " " << std::endl;
+	}
+}
 
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "russian");
-	int i = 3;
-	address* address_array = new address[3];
-	for (int i = 0; i < 3; i++) {
+	int n = 3;
+	address* address_array = new address[n];
+	for (int i = 0; i < n; i++) {
 		std::cout << "Введите название города:  ";
-		std::cin >> address_array[i].town;
+		std::cin >> address_array -> town[i];
 		std::cout << std::endl;
 		std::cout << "Введите название улицы:  ";
-		std::cin >> address_array[i].strit;
+		std::cin >> address_array[i].strit[i];
 		std::cout << std::endl;
 		std::cout << "Введите номер дома:  ";
-		std::cin >> address_array[i].home;
+		std::cin >> address_array[i].home[i];
 		std::cout << std::endl;
 		std::cout << "Введите номер квартиры:  ";
-		std::cin >> address_array[i].apartment;
+		std::cin >> address_array[i].apartment[i];
 		std::cout << std::endl;
 		std::cout << "Введите индекс:  ";
-		std::cin >> address_array[i].index;
+		std::cin >> address_array[i].index[i];
 		std::cout << std::endl;
-				
+
 	}
-	for (int i = 0; i < 3; i++) {
-		std::cout << "Город: " << address_array[i].town << std::endl;
-		std::cout << "Улица: " << address_array[i].strit << std::endl; 
-		std::cout << "Номер дома: " << address_array[i].home << std::endl; 
-		std::cout << "Номер квартиры: " << address_array[i].apartment << std::endl; 
-		std::cout << "Индекс: " << address_array[i].index << std::endl;
-		std::cout << " " << std::endl;
-	}
+	print(address_array[n]);
+	delete[] address_array;
 	return 0;
-}
+} 
